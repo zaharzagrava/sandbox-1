335af81c00ef338da21806a888e95e0a2d070127
Ruslan Plastun <zaharzagrava@gmail.com>
2020-10-23 05:31:02 -0400

    changed to scss odules

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,9 +1,11 @@
 import React, { ReactElement } from 'react';
 
+import styles from './Login.module.scss';
+
 interface Props {}
 
 function Login({}: Props): ReactElement {
-  return <div>Login</div>;
+  return <div className={styles.container}></div>;
 }
 
 export default Login;
